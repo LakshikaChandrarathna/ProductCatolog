73a69da53f3d0987509d64a4d55d6b277d7754b4
lakshika chandrarathna <ulakshika.c1218@gmail.com>
2025-04-23 13:20:44 +0530

    changes done

--- src/main/java/com/example/productcatalog/controller/CategoryController.java
+++ src/main/java/com/example/productcatalog/controller/CategoryController.java
@@ -3,10 +3,13 @@ package com.example.productcatalog.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
-    public List<Category> getAllCategories(){
 
+    private CategoryService categoryService;
+    public List<Category> getAllCategories(){
+    return categoryService.getAllCategories();
     }
 }
--- src/main/java/com/example/productcatalog/service/CategoryService.java
+++ src/main/java/com/example/productcatalog/service/CategoryService.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryService {
     private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<category> getAllCategories(){
+        return
+    }
 }
